e084fe15a56c1eecdd309035ff8a71f9b7e3cd81
elisei <brunoelisei@o2ti.com>
2021-11-29 13:03:42 -0300

    O2TI 🖤 Magento
    Magento Version 2.3.x

---
+++ Plugin/MSPReCaptchaOnepage.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Copyright © O2TI. All rights reserved.
+ * @author    Bruno Elisei <brunoelisei@o2ti.com>
+ * See COPYING.txt for license details.
+ */
+
+namespace O2TI\CheckoutIdentificationStep\Plugin;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use MSP\ReCaptcha\Block\LayoutProcessor\Checkout\Onepage;
+use Magento\ReCaptchaUi\Model\IsCaptchaEnabledInterface;
+use Magento\ReCaptchaUi\Model\UiConfigResolverInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use MSP\ReCaptcha\Model\Config as MSPConfig;
+use MSP\ReCaptcha\Model\LayoutSettings;
+use O2TI\CheckoutIdentificationStep\Helper\Config;
+
+/**
+ *  MSPReCaptchaOnepage - MSP Insert ReCaptcha to Step Identification.
+ */
+class MSPReCaptchaOnepage
+{
+    /**
+     * @var LayoutSettings
+     */
+    protected $layoutSettings;
+
+    /**
+     * @var MSPConfig
+     */
+    protected $mspConfig;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    /**
+     * @var Config
+     */
+    protected $config;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     * @param LayoutSettings $layoutSettings
+     * @param MSPConfig $mspConfig
+     * @param Config $config
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        LayoutSettings $layoutSettings,
+        MSPConfig $mspConfig,
+        Config $config
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->layoutSettings = $layoutSettings;
+        $this->mspConfig = $mspConfig;
+        $this->config = $config;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function aroundProcess(Onepage $layoutProcessor, callable $proceed, ...$args): array
+    {
+        $jsLayout = $proceed(...$args);
+        if ($this->config->isEnabled()) {
+
+            if ($this->mspConfig->isEnabledFrontend()) {
+                $key = 'customer_login';
+                 // phpcs:ignore
+                $jsLayout['components']['checkout']['children']['steps']['children']['identification-step']['children']['identification']['children']['msp_recaptcha']['settings'] = $this->layoutSettings->getCaptchaSettings();
+            } else {
+                 // phpcs:ignore
+                if (isset($jsLayout['components']['checkout']['children']['steps']['children']['identification-step']['children']['identification']['children']['msp_recaptcha'])
+                ) {
+                     // phpcs:ignore
+                    unset($jsLayout['components']['checkout']['children']['steps']['children']['identification-step']['children']['identification']['children']['msp_recaptcha']);
+                }
+            }
+            $layoutProcessor = $layoutProcessor;
+        }
+
+        return $jsLayout;
+    }
+}
